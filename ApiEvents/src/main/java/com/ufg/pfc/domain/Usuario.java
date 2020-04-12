732495d930cfadbde5a71b4df19895f5d98390bd
marciopalves <marciopalves89@gmail.com>
2020-04-12 08:02:06 -0300

    eventos01 - Utilizando constantes nas classes Services; Mudando id do Usuario de integer para Long.

--- ApiEvents/src/main/java/com/ufg/pfc/domain/Usuario.java
+++ ApiEvents/src/main/java/com/ufg/pfc/domain/Usuario.java
@@ -19,7 +19,7 @@ public class Usuario implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
-	private Integer id;
+	private Long id;
 	
 	@JsonInclude(Include.NON_NULL)	
 	private String nome;
@@ -44,10 +44,10 @@ public class Usuario implements Serializable {
 		
 	}	
 	
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getNome() {
---
+++ ApiEvents/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+﻿-- Usuários
+insert into usuario(id, nome, cpf, email, telefone) values (1, 'José Diniz', '11111111111', 'jose365@hotmail.com', '6235212112');
+insert into usuario(id, nome, cpf, email, telefone) values (2, 'Marcio da Silva', '22222222222', 'teste@hotmail.com', '6235212444');
+insert into usuario(id, nome, cpf, email, telefone) values (3, 'Francisco Tolentino', '3333333333', 'fran@hotmail.com', '6235212444');
+
+-- Profissões
+insert into profissao(id, nome) values (1, 'Garçon');
+insert into profissao(id, nome) values (2, 'Cozinheiro');
+insert into profissao(id, nome) values (3, 'Churrasqueiro');
+
+
+
